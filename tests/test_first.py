ad29c5015575ab08cca5bae02732baaa5e11af85
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-14 19:46:15 +0900

    테스트파일 리눅스와 맥 분기 나누고 테스트

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,17 +1,32 @@
-from check_os_ver.osver import get_os_version_of_mac
+import pytest
+import sys
+from check_os_ver.osver import get_os_version_of_mac, get_os_version_of_linux
+
+@pytest.mark.skipif(sys.platform != "darwin", reason="Only runs on macOS")
+def test_on_mac():
+    print("Running on macOS")
+    v = get_os_version_of_mac()
+    assert "Darwin" in v
+
+@pytest.mark.skipif(not sys.platform.startswith("Linux"), reason="Only runs on Linux")
+def test_on_linux(v):
+    print("Running on Linux")
+    v = get_os_version_of_linux()
+    assert "LTS" in v
 
 def test_first():
-    v = get_os_version_of_mac()
+    if sys.platform == "darwin":
+        v = get_os_version_of_mac()
+    elif sys.platform.startswith("Linux"):
+        v = get_os_version_of_linux()
+    else:
+        pytest.skip("Unsupported platform")
 
     # 정상적인 값인지
     assert v is not None
 
     # 빈 문자열이 아닌지
     assert v != ""
-
-    # 문자열에 LTS가 포함 되었는지 -> mac이기 때문에, Darwin이 포함 되었는지로 변경
-    # assert "LTS" in v, "'LTS' is not in "
-    assert "Darwin" in v
 
     # 문자열에 문자도 있고, 숫자도 있는지
     assert any(c.isalpha() for c in v)
@@ -24,3 +39,4 @@ def test_first():
     assert len(v) > 6
 
     # 기타 등등
+    print("\nAll test passed successfully!")
